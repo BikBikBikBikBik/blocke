635b4d340b425965fa77f9e70c3eee5ea3c5bb60
jose edinaldo <edinaldofox@gmail.com>
2018-09-19 18:09:24 -0300

    create switch for function convertObject

--- bin/blocke-api.js
+++ bin/blocke-api.js
@@ -40,46 +40,43 @@ function convertObject(res) {
     var blockApi = new Object();
 
     for (var i = 0; i < res.length; i++) {
-        if (res[i].option == 'Account') {
-            var account = new Object();
-            account.address = res[i].data._address;
-            account.balance = res[i].data._confirmedBalance;
-            account.tokenBalances = res[i].data._tokenBalances;
-            account.unconfirmedBalance = res[i].data._unconfirmedBalance;
-            blockApi.account = account;
-        }
 
-        if (res[i].option == 'Block') {
-            var block = new Object();
+        switch (res[i].option) {
+            case 'Account':
+                var account = new Object();
+                account.address = res[i].data._address;
+                account.balance = res[i].data._confirmedBalance;
+                account.tokenBalances = res[i].data._tokenBalances;
+                account.unconfirmedBalance = res[i].data._unconfirmedBalance;
+                blockApi.account = account;
+                break;
+            case 'Block':
+                var block = new Object();
 
-            block.difficulty = res[i].data._difficulty;
-            block.hash = res[i].data._hash;
-            block.number = res[i].data._number;
-            block.time = res[i].data._time;
-            block.transactionCount = res[i].data._transactionCount;
-            blockApi.block = block;
-        }
+                block.difficulty = res[i].data._difficulty;
+                block.hash = res[i].data._hash;
+                block.number = res[i].data._number;
+                block.time = res[i].data._time;
+                block.transactionCount = res[i].data._transactionCount;
+                blockApi.block = block;
+                break;
+            case 'Transaction':
+                var transaction = new Object();
 
-        if (res[i].option == 'Transaction') {
-            var transaction = new Object();
-
-            transaction.amountSent = res[i].data._amountSent;
-            transaction.blockHash = res[i].data._blockHash;
-            transaction.hash = res[i].data._hash;
-            transaction.recipients = res[i].data._recipients;
-            transaction.senders = res[i].data._senders;
-            transaction.time = res[i].data._time;
-            blockApi.transaction = transaction;
-
-        }
-
-        if (res[i].option == 'Network Info') {
-            var network = new Object();
-            network.difficulty = res[i].data._difficulty;
-            network.hashRate = res[i].data._hashRate;
-            network.height = res[i].data._height;
-            network.lastBlockTime = res[i].data._lastBlockTime;
-            blockApi.network = network;
+                transaction.amountSent = res[i].data._amountSent;
+                transaction.blockHash = res[i].data._blockHash;
+                transaction.hash = res[i].data._hash;
+                transaction.recipients = res[i].data._recipients;
+                transaction.senders = res[i].data._senders;
+                transaction.time = res[i].data._time;
+                blockApi.transaction = transaction;
+            case 'Network Info':
+                var network = new Object();
+                network.difficulty = res[i].data._difficulty;
+                network.hashRate = res[i].data._hashRate;
+                network.height = res[i].data._height;
+                network.lastBlockTime = res[i].data._lastBlockTime;
+                blockApi.network = network;
         }
     }
 
